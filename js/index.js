a46acdd790757cc7027898918b125e584c368e83
marksv21 <marvmms@gmail.com>
2022-11-18 18:53:35 -0300

    formulário com 5 variaveis

--- js/index.js
+++ js/index.js
@@ -1,12 +1,37 @@
+// leitura e captação dos formulários no console
 var email = document.getElementById('email');
 var password = document.getElementById('password');
+var nome = document.getElementById('nome');
+var sobrenome = document.getElementById('sobrenome');
+var fone = document.getElementById('fone');
 
+const sss = document.querySelector("#send");
+sss.addEventListener("click",function(f){
+    f.preventDefault();
+    const nome = document.querySelector("#nome");
+    const value = nome.value;
+    console.log(value);
+})
+const aaa = document.querySelector("#send");
+aaa.addEventListener("click",function(g){
+    g.preventDefault();
+    const sobrenome = document.querySelector("#sobrenome");
+    const value = sobrenome.value;
+    console.log(value);
+})
 const btn = document.querySelector("#send");
-
 btn.addEventListener("click",function(e){
     e.preventDefault();
     const email = document.querySelector("#email");
-    const value =email.value;
+    const value = email.value;
+    console.log(value);
+})
+
+const nnn = document.querySelector("#send");
+nnn.addEventListener("click",function(l){
+    l.preventDefault();
+    const fone = document.querySelector("#fone");
+    const value =fone.value;
     console.log(value);
 })
 const rtt = document.querySelector("#send");
@@ -15,8 +40,23 @@ rtt.addEventListener("click",function(d){
     const password = document.querySelector("#password");
     const value = password.value;
     console.log(value);
-
 })
+
+// destacar os caixas do formulário
+nome.addEventListener('focus',()=>{
+nome.style.borderColor = "#000000"});
+nome.addEventListener('blur',()=>{
+nome.style.borderColor = "#ff0000"});
+
+sobrenome.addEventListener('focus',()=>{
+sobrenome.style.borderColor = "#000000"});
+sobrenome.addEventListener('blur',()=>{
+sobrenome.style.borderColor = "#ff0000"});
+
+fone.addEventListener('focus',()=>{
+fone.style.borderColor = "#000000"});
+fone.addEventListener('blur',()=>{
+fone.style.borderColor = "#ff0000"});
 
 email.addEventListener('focus',()=>{
 email.style.borderColor = "#000000"});
